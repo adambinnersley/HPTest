eb7b25b9479f66b6c422cba34c9d1cff66f59eb8
Adam Binnersley <abinnersley@gmail.com>
2017-09-01 16:31:56 +0100

    Update database connection

--- tests/HazardPerceptionTest.php
+++ tests/HazardPerceptionTest.php
@@ -8,13 +8,17 @@ use HPTest\HazardPerception;
 use PHPUnit\Framework\TestCase;
 
 class HazardPerceptionTest extends TestCase{
-    
     protected static $db;
     protected static $user;
     protected static $hp;
     
     public function setUp() {
-        self::$db = new Database('localhost', 'root', '', 'hp_test');
+        self::$db = new Database('localhost', $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'], $GLOBALS['DB_DBNAME']);
+        if(!self::$db->isConnected()){
+             $this->markTestSkipped(
+                'No local database connection is available'
+            );
+        }
         self::$user = new User(self::$db);
         self::$hp = new HazardPerception(self::$db, new Smarty(), self::$user);
         parent::setUp();
